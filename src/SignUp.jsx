503b7e0ad20ea03126d19964f37ff63f45b1c936
Seriko <serdarash@gmail.com>
2021-12-10 23:05:02 -0500

    update

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -62,67 +62,67 @@ const SignUp = () => {
   // reCAPTCHA v3 integration for me info check below links
   //www.cluemediator.com/how-to-implement-recaptcha-v3-in-react
 
-  //   useEffect(() => {
-  //     const loadScriptByURL = (id, url, callback) => {
-  //       const isScriptExist = document.getElementById(id);
+  useEffect(() => {
+    const loadScriptByURL = (id, url, callback) => {
+      const isScriptExist = document.getElementById(id);
 
-  //       if (!isScriptExist) {
-  //         var script = document.createElement('script');
-  //         script.type = 'text/javascript';
-  //         script.src = url;
-  //         script.id = id;
-  //         script.onload = function () {
-  //           if (callback) callback();
-  //         };
-  //         document.body.appendChild(script);
-  //       }
+      if (!isScriptExist) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        script.id = id;
+        script.onload = function () {
+          if (callback) callback();
+        };
+        document.body.appendChild(script);
+      }
 
-  //       if (isScriptExist && callback) callback();
-  //     };
+      if (isScriptExist && callback) callback();
+    };
 
-  //     // load the script by passing the URL
-  //     loadScriptByURL(
-  //       'recaptcha-key',
-  //       `https://www.google.com/recaptcha/api.js?render=${SITE_KEY}`,
-  //       function () {
-  //         console.log('Script loaded!');
-  //       }
-  //     );
-  //   }, []);
+    // load the script by passing the URL
+    loadScriptByURL(
+      'recaptcha-key',
+      `https://www.google.com/recaptcha/api.js?render=${SITE_KEY}`,
+      function () {
+        console.log('Script loaded!');
+      }
+    );
+  }, []);
 
-  //   const handleCreate = (e) => {
-  //     e.preventDefault();
-  //     setLoading(true);
-  //     window.grecaptcha.ready(() => {
-  //       window.grecaptcha
-  //         .execute(SITE_KEY, { action: 'submit' })
-  //         .then((token) => {
-  //           submitData(token);
-  //         });
-  //     });
-  //   };
+  const handleCreate = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    window.grecaptcha.ready(() => {
+      window.grecaptcha
+        .execute(SITE_KEY, { action: 'submit' })
+        .then((token) => {
+          submitData(token);
+        });
+    });
+  };
 
-  //   // this part requests token verify from backend server
+  // this part requests token verify from backend server
 
-  //   const submitData = (token) => {
-  //     // call a backend API to verify reCAPTCHA response
-  //     fetch('http://localhost:4000/verify', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         email: email,
-  //         password: password,
-  //         'g-recaptcha-response': token,
-  //       }),
-  //     })
-  //       .then((res) => res.json())
-  //       .then((res) => {
-  //         setLoading(false);
-  //         setResponse(res);
-  //       });
-  //   };
+  const submitData = (token) => {
+    // call a backend API to verify reCAPTCHA response
+    fetch('http://localhost:4000/verify', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        'g-recaptcha-response': token,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setLoading(false);
+        setResponse(res);
+      });
+  };
 
   return (
     <div className="signUpContainer">
